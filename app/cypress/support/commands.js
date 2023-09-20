c104c24b9b17ccd5eaa3705cf508e66bcb458c3c
Marley <marley.ruby.on.rails@gmail.com>
2023-09-20 10:18:05 +0300

    Update commands.js

--- app/cypress/support/commands.js
+++ app/cypress/support/commands.js
@@ -26,3 +26,46 @@
 
 /// <reference types"Cypress" />
 /// <reference types"cypress-xpath" />
+
+// Custom command for clicking on link using label
+
+Cypress.Commands.add('clickLink', (label) => {
+  cy.get('a').contains(label).click();
+});
+
+// Overwrite contains
+
+// Cypress.Commands.overwriteQuery(
+//   'contains',
+//   function (contains, filter, text, userOptions = {}) {
+//     // This is parameter resolution from Cypress v12.7.0 source
+//     if (Cypress._.isRegExp(text)) {
+//       // .contains(filter, text)
+//       // Do nothing
+//     } else if (Cypress._.isObject(text)) {
+//       // .contains(text, userOptions)
+//       userOptions = text;
+//       text = filter;
+//       filter = '';
+//     } else if (Cypress._.isUndefined(text)) {
+//       // .contains(text)
+//       text = filter;
+//       filter = '';
+//     }
+
+//     userOptions.matchCase = false;
+
+//     let contains0 = contains.bind(this); // this line fixes the error
+
+//     return contains0(filter, text, userOptions);
+//   }
+// );
+
+//  Custom command for login
+
+Cypress.Commands.add('loginapp', (email, password) => {
+  cy.get('#Email').type(email);
+  cy.get('#Password').type(password);
+  // cy.get("button[class='button-1 login-button]").click();
+  cy.get('form > .buttons > .button-1').click();
+});
