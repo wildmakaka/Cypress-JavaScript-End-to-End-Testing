a871596bcc21473d8c9d7033d03d0155e6755923
Marley <marley.ruby.on.rails@gmail.com>
2023-09-20 12:15:45 +0300

    Create CaptureScreenshotsAndVideos.cy.js

---
+++ app/cypress/e2e/CaptureScreenshotsAndVideos.cy.js
@@ -0,0 +1,18 @@
+describe('Capture Screenshots & Videos', () => {
+  it.skip('[OK!] Capture Screenshots & Videos', () => {
+    cy.visit('https://demo.opencart.com');
+    cy.screenshot('homepage');
+
+    cy.wait(3000);
+    cy.get("img[title='Your Store']").screenshot('logo');
+  });
+
+  it('[OK!] Automatically capture screenshot & video on failure ', () => {
+    cy.visit('https://demo.opencart.com');
+
+    cy.get('li:nth-child(7) a:nth-child(1)').click();
+    cy.get("div[id='content'] h2").should('have.text', 'Tablets');
+  });
+
+  //
+});
